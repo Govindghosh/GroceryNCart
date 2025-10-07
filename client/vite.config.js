3b660284189e9de55886f67cd5ed8c386c8ea39d
Govind ghosh <govindghosh0@gmail.com>
2025-10-07 12:44:57 +0530

    [CONFIG]Vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": import.meta.env.VITE_API_URL || "http://localhost:3000", // backend server
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_') // Node env
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': env.VITE_API_URL || 'http://localhost:3000',
+      },
     },
-  },
+  }
 })
